3051f89354392ba28af685f8fe069adfe5f25c86
Mayank <albertnike1371@gmail.com>
2025-05-20 19:40:29 +0530

    setup cloudinary and multer

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -56,8 +56,7 @@ userSchema.methods.isPasswordCorrect = async function(password) {
     return await bcrypt.compare(password, this.password)
 }
 userSchema.methods.generateAccessToken = function() {
-    return 
-        jwt.sign(
+    return jwt.sign(
             {
                 _id: this._id,
                 email: this.email,
@@ -66,19 +65,18 @@ userSchema.methods.generateAccessToken = function() {
             },
             process.env.ACCESS_TOKEN_SECRET,
             {
-                expiresIn:ACCESS_TOKEN_EXPIRY
+                expiresIn:process.env.ACCESS_TOKEN_EXPIRY
             }
         )
 }
 userSchema.methods.generateRefreshToken = function() {
-    return 
-        jwt.sign(
+    return jwt.sign(
             {
                 _id: this._id,
             },
             process.env.REFRESH_TOKEN_SECRET,
             {
-                expiresIn:REFRESH_TOKEN_EXPIRY,
+                expiresIn:process.env.REFRESH_TOKEN_EXPIRY,
             }
         )
 }
